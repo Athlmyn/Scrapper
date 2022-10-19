efa96730aa246b4ce127f2b226a3d1c229530ccf
Athlmyn <Athlmyn@Outlook.com>
2022-10-18 21:59:56 -0600

    Job Elements

--- src/main.py
+++ src/main.py
@@ -9,6 +9,13 @@ soup = bs4.BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="ResultsContainer")
 
+job_elements = results.find_all("div", class_="card-content")
+
+for job_element in job_elements:
+    print(job_element, end="\n"*2)
+
 #print(page.content)
-print(soup.get_text())
+#print(soup.get_text())
+#print(results.prettify())
+
 
